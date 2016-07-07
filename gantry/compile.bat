ea3c72f847d912bcf1e84a5d0fd626db67f9425c
Paul Chase <paenian@gmail.com>
2016-07-07 18:58:53 -0400

    Fixed the endcap cuts - it was missing the belt slots!
    Paul

--- gantry/compile.bat
+++
@@ -1,6 +0,0 @@
-openscad gantry.scad -D part=0 -o hotend_carriage.stl
-openscad gantry.scad -D part=1 -o gantry_clamp.stl
-openscad gantry.scad -D part=2 -o gantry_end.stl
-openscad gantry.scad -D part=3 -o gantry_carriage.stl
-openscad gantry.scad -D part=4 -o belt_stretcher.stl
-openscad gantry.scad -D part=5 -o hotend_mount.stl
